ed31e1ad830598d8955f85a2559bf74c9d276a69
Sophia <sopsloan@gmail.com>
2024-06-06 23:51:04 -0700

    sigh

--- final-project/doll-creator.js
+++ final-project/doll-creator.js
@@ -1,7 +1,7 @@
 const ids = ["h", "f", "t", "b", "s", "a"];
 const buttonids = ["hb", "fb", "tb", "bb", "sb", "ab"];
 
-function displayList(name, buttonid) {
+function displayNavbar(name, buttonid) {
     var element = document.getElementById(name);
     element.style.display = "block";
     element = document.getElementById(buttonid);
@@ -16,4 +16,54 @@ function displayList(name, buttonid) {
             element.classList.remove("toggled");
         }
     }
+}
+
+function setClothingItem(clothing_type, clothing_name) {
+    console.log(clothing_type);
+    console.log(clothing_name);
+
+    var img = document.getElementById(clothing_type);
+    img.src = "images/customization/".concat(clothing_name);
+    
+}
+
+// Display all clothing of clothing_type
+function displayOptions(clothing_type, names) {
+    var div = document.getElementById(clothing_type);
+
+    var length = names.length;
+    var cur_element;
+    for (i = 0; i < length; i++) {
+        if (i % 3 == 0) {
+            cur_element = document.createElement("div");
+            cur_element.classList.add("clothes_row");
+            div.appendChild(cur_element);
+        }
+        var img = document.createElement("img");
+        img.src = "images/customization/icons/".concat(names[i]);
+        img.className = "icon";
+        img.addEventListener('click', setClothingItem.bind(null, clothing_type.concat("d"), names[i]));
+        cur_element.appendChild(img);
+    }
+}
+
+function download() {
+    
+    let canvas = document.getElementById("mycanvas");
+    let ctx = canvas.getContext("2d");
+
+    var length = ids.length;
+    for (i = 0; i < length; i++) {
+        var img = document.getElementById(ids[i].concat("d"));
+        if (img != null) {
+            console.log("hi");
+            ctx.drawImage(img, 0, 0);
+        }
+    }
+
+    let link = document.getElementById("download");
+    console.log("ugh");
+    link.download = 'doll.png';
+    link.href = document.getElementById('mycanvas').toDataURL();
+    link.click();
 }
